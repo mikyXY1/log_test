f57ae461f45427b36546d603604a4369943f9622
Miky <mm1234@digitm.net>
2025-11-14 18:43:48 +0100

    uprava

--- testik.sh
+++ testik.sh
@@ -9,3 +9,18 @@ x=${name}
 echo "${y} a ${x}"
 
 echo "hodnota y je ${y} a hodnota x je ${x}"
+
+echo "====================================================================================="
+
+porovnani_cisel() {
+prvni=$1
+druhe=$2
+
+  if [[ prvni -eq druhe ]]; then
+        echo "cisla se rovnaji"
+  else
+        echo "cisla se nerovnaji"
+  fi
+}
+
+porovnani_cisel $1 $2 # volani fce s parametry, ktere jsou predany pri spusteni skriptu testik.sh
