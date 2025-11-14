0c03146aea73f2b901951b0cf7bd897b4041cd32
Miky <mm1234@digitm.net>
2025-11-14 18:24:54 +0100

    update

---
+++ check.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+echo "-------"
+
+name="server_log.txt"
+
+echo "-------"
+for type_message in "INFO" "WARNING" "ERROR" #vyčet zpráv podle typu
+do
+    count=$(cat ${name} | grep -E "] ${type_message}" | wc -l)   #počítání výskytů podle typu zprávy
+    echo "Zpravy typu: ${type_message} - pocet: ${count}"        #výpis počtu výskytů podle typu zprávy  
+done
+
+echo "-------"
+seznam=$(cat ${name} | grep -E "logged in" | sed -e "s/.*  User '\(.*\)'.*/\1/g")  #výpis uživatelů, kteří se přihlásili
+echo "Seznam všech uživatelů, kteří se přihlásili:"
+echo "${seznam}"
+#echo -e "Seznam všech uživatelů, kteří se přihlásili: \n${seznam}"
---
+++ testik.sh
@@ -0,0 +1,11 @@
+cislo=123
+
+name="karel"
+
+y=${cislo}
+
+x=${name}
+
+echo "${y} a ${x}"
+
+echo "hodnota y je ${y} a hodnota x je ${x}"
